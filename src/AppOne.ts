8fad1200922260bf630edbc50dc459db31872a5e
Zedro <45104292+PedroZappa@users.noreply.github.com>
2025-05-15 11:14:49 +0100

    feat(babylonjs): integrate Babylon.js addons and core libraries
    Added @babylonjs/addons and @babylonjs/core to enhance Babylon.js functionalities. Updated AppOne.ts to utilize HtmlMeshRenderer and HtmlMesh for advanced rendering. Adjusted tsconfig.json for compatibility and improved TypeScript configuration. Updated .editorconfig to standardize indentation for JavaScript and TypeScript files.
    
    Signed-off-by: Zedro <45104292+PedroZappa@users.noreply.github.com>

--- src/AppOne.ts
+++ src/AppOne.ts
@@ -1,47 +1,51 @@
 import * as BABYLON from 'babylonjs'
+import {
+    HtmlMeshRenderer, HtmlMesh
+} from "@babylonjs/addons/htmlMesh";
 export class AppOne {
-    engine: BABYLON.Engine;
-    scene: BABYLON.Scene;
+    private _engine: BABYLON.Engine;
+    private _scene: BABYLON.Scene;
 
     constructor(readonly canvas: HTMLCanvasElement) {
-        this.engine = new BABYLON.Engine(canvas)
+        this._engine = new BABYLON.Engine(canvas)
         window.addEventListener('resize', () => {
-            this.engine.resize();
+            this._engine.resize();
         });
-        this.scene = createScene(this.engine, this.canvas)
+        this._scene = createScene(this._engine, this.canvas)
 
     }
 
     debug(debugOn: boolean = true) {
         if (debugOn) {
-            this.scene.debugLayer.show({ overlay: true });
+            this._scene.debugLayer.show({ overlay: true });
         } else {
-            this.scene.debugLayer.hide();
+            this._scene.debugLayer.hide();
         }
     }
 
     run() {
         this.debug(true);
-        this.engine.runRenderLoop(() => {
-            this.scene.render();
+        this._engine.runRenderLoop(() => {
+            this._scene.render();
         });
     }
-
-
-
 }
 
 
-var createScene = function (engine: BABYLON.Engine, canvas: HTMLCanvasElement) {
+var createScene = function(_engine: BABYLON.Engine, canvas: HTMLCanvasElement) {
     // this is the default code from the playground:
 
     // This creates a basic Babylon Scene object (non-mesh)
-    var scene = new BABYLON.Scene(engine);
+    const scene = new BABYLON.Scene(_engine);
+    scene.clearColor = new BABYLON.Color4(0, 0, 0, 0);
+    // this._scene.createDefaultCameraOrLight(true, true, true);
+
+    // const htmlMeshRenderer = new HtmlMeshRenderer(this._scene);
 
     // This creates and positions a free camera (non-mesh)
     var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
 
-    // This targets the camera to scene origin
+    // This targets the camera to this._scene origin
     camera.setTarget(BABYLON.Vector3.Zero());
 
     // This attaches the camera to the canvas
@@ -53,12 +57,6 @@ var createScene = function (engine: BABYLON.Engine, canvas: HTMLCanvasElement) {
     // Default intensity is 1. Let's dim the light a small amount
     light.intensity = 0.7;
 
-    // Our built-in 'sphere' shape.
-    var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 2, segments: 32 }, scene);
-    // Move the sphere upward 1/2 its height
-    let startPos = 2;
-    sphere.position.y = startPos;
-
     // Our built-in 'ground' shape.
     var ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
     var groundMaterial = new BABYLON.StandardMaterial("groundMaterial", scene);
@@ -66,29 +64,6 @@ var createScene = function (engine: BABYLON.Engine, canvas: HTMLCanvasElement) {
     ground.material = groundMaterial;
     groundMaterial.bumpTexture = new BABYLON.Texture("./normal.jpg", scene);
     //groundMaterial.bumpTexture.level = 0.125;
-
-
-    var redMaterial = new BABYLON.StandardMaterial("redMaterial", scene);
-    redMaterial.diffuseColor = new BABYLON.Color3(1, 0, 0); // RGB for red
-    sphere.material = redMaterial;
-
-    var sphereVelocity = 0;
-    var gravity = 0.009;
-    var reboundLoss = 0.1;
-
-    scene.registerBeforeRender(() => {
-        sphereVelocity += gravity;
-        let newY = sphere.position.y - sphereVelocity;
-        sphere.position.y -= sphereVelocity
-        if (newY < 1) {
-            sphereVelocity = (reboundLoss - 1) * sphereVelocity;
-            newY = 1;
-        }
-        sphere.position.y = newY;
-        if (Math.abs(sphereVelocity) <= gravity && newY < 1 + gravity) {
-            sphere.position.y = startPos++;
-        }
-    });
 
     return scene;
 };
